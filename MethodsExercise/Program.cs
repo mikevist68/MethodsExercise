da8d63fd2f3a1a7dccbf1a08038953f8d551c184
Michael Vistocci <mikevist68@yahoo.com>
2023-05-12 18:09:34 -0400

    completed work

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,89 @@
-﻿namespace MethodsExercise
+﻿using System.ComponentModel;
+using System.Net.Mail;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MethodsExercise
 {
     public class Program
     {
-        static void Main(string[] args)
+
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MadLibs();
+
+            int myAddedVariable = Add(2, 2);
+            Console.WriteLine(myAddedVariable);
+
+            int mySubtractedVariable = Subtract(2, 2);
+            Console.WriteLine(mySubtractedVariable);
+
+            int myMultipliedVariable = Multiply(2, 2);
+            Console.WriteLine(myMultipliedVariable);
+
+            int myModulusVariable = Modulus(2, 2);
+            Console.WriteLine(myModulusVariable);
+
+            int myDivideVariable = Divide(2, 2);
+            Console.WriteLine(myDivideVariable);
         }
-    }
-}
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+            return num1 % num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            return (num1 / num2);
+        }
+
+        public static void MadLibs() 
+        {
+            Console.WriteLine("Whats your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Whats your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("Whats your favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("Whats your favorite band?");
+            string band = Console.ReadLine();
+
+            Console.WriteLine($"Whats your name? {name} Whats your favorite color? {color} Whats your favorite animal? {animal} Whats your favoite band? {band}");
+
+        }
+    }   
+        
+    
+
+}            
+       
+
+
+
+        
+        
+
+    
+
+
+    
+
